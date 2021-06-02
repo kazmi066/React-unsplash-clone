1d5a101357af0a514a6378881be49056d6222f78
kazmi066 <awaisabbas47@gmail.com>
2021-06-02 12:35:09 +0500

    lazy load

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,9 +1,13 @@
-import React, { useContext, useEffect } from "react";
+import React, { Suspense, useContext, useEffect } from "react";
 import "./Home.scss";
 import Header from "../../components/Header/Header";
 import Main from "../../components/Main/Main";
 import SplashGrid from "../../components/SplashGrid/SplashGrid";
 import { UnsplashContext } from "../../context";
+
+// const SplashGrid = React.lazy(() =>
+//   import("../../components/SplashGrid/SplashGrid")
+// );
 
 export default function Home() {
   const { GetImages } = useContext(UnsplashContext);
